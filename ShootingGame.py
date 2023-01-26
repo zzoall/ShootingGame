02d0302006f0ff469055c52ab39a42bc32bebaa4
HyoJoon-Yang <hyojoon.tech@gmail.com>
2023-01-26 20:56:29 +0900

    Update ShootingGame.py
    Edit writeScore, writePassed

--- ShootingGame.py
+++ ShootingGame.py
@@ -41,6 +41,16 @@ def drawObject(obj, x, y):
 # 운석을 맞춘 개수 계산
 def writeScore(count):
     global gamePad
+    font = pygame.font.Font('NanumGothic.ttf', 20)
+    text = font.render('파괴한 운석 수:',str(count), True, (255,255,255))
+    gamePad.blit(text, (10,0))
+
+# 운석을 놓친 개수 계산
+def writePassed(count):
+    global gamePad
+    font = pygame.font.Font('NanumGothic.ttf', 20)
+    text = font.render('파괴한 운석 수:',str(count), True, (255,0,0))
+    gamePad.blit(text, (350,0))
 
 # 운석이 화면 아래로 통과한 개수
 def writePassed(count):
